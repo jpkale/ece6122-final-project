7ea001c4085f1ee136e2b13d4c0b823e5a8e9b19
jpkale <joseph.p.kale@gmail.com>
2018-04-24 15:34:06 -0400

    Removed Request class

--- client/ui/driver.cpp
+++ client/ui/driver.cpp
@@ -10,8 +10,8 @@ using namespace std;
 Driver::Driver(
     bool (*login_cb)(Credential*),
     bool (*signup_cb)(Credential*),
-    bool (*deposit_cb)(Request*),
-    bool (*withdrawl_cb)(Request*)
+    bool (*deposit_cb)(double),
+    bool (*withdrawl_cb)(double)
 ) {
     this->login_cb = login_cb;
     this->signup_cb = signup_cb;
