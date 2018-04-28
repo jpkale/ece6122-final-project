30ebf0bbec119ae5403aca182245348bbc35c54c
Nick Dargi <ndargi@bu.edu>
2018-04-28 12:21:04 -0400

    adding Socket Directory

---
+++ Sockets/Serialization/Serialization.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Nick Dargi on 4/24/18.
+//
+
+#include "Serialization.h"
+char* serializeinput()
+{
+
+}
+char* serializedeposit()
+{
+
+}
+char* serializewithdrawl()
+{
+
+}
---
+++ Sockets/Serialization/Serialization.h
@@ -0,0 +1,10 @@
+//
+// Created by Nick Dargi on 4/24/18.
+//
+
+#ifndef SERIALIZATIONCLASS_SERIALIZATION_H
+#define SERIALIZATIONCLASS_SERIALIZATION_H
+char* serializeaccount();
+char* serializewithdrawl();
+char* serializedeposit();
+#endif //SERIALIZATIONCLASS_SERIALIZATION_H
