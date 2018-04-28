6f49d0b3494ab79a3cda914bd4517422fddd2c99
Dennis Soasti <soastid@gmail.com>
2018-04-27 21:29:24 -0400

    Finished main classes. Defined all member equations. Need to review with team for adaptation

--- account.h
+++ account.h
@@ -3,27 +3,31 @@
 
 #endif //BANKINGAPPSERVER_ACCOUNT_H
 
+#include <string>
+#include "accountHolder.h"
+
 //Manage account information from login credentials to balance
 class accountHolder;
 class account {
 public:
     //Constructors:
     account();
-    account(double accountNumber, double balance, const char* userID, const char* userPassword);
+    account(double accountNumberIn, double balanceIn, const char* userIDIn, const char* userPasswordIn);
     //overloaded operators
 
     //member functions
-    void linkAccount(double accountNumber, accountHolder* a); //associates account number(s) with an account holder
+    void linkAccount(double accountNumber, accountHolder a); //associates account number(s) with an account holder
     void deposit(double accountNumber, double depositAmount);
     void withdrawal(double accountNumber, double withdrawAmount);
 //Data members
 public:
     double balance;
     double accountNumber;
+    std::string userID;
 
 private:
     //key for password encryption if needed or other private encryption data members
-    const char* key;
-    const char* userPassword;
+    std::string key;
+    std::string userPassword;
 
 };
--- accountHolder.h
+++ accountHolder.h
@@ -2,6 +2,7 @@
 #define BANKINGAPPSERVER_ACCOUNTHOLDER_H
 
 #endif //BANKINGAPPSERVER_ACCOUNTHOLDER_H
+#include <string>
 
 class account;
 
@@ -10,27 +11,26 @@ public:
     //Constructors:
     accountHolder();
     accountHolder(const char* nameU, const char* middleInitU, const char* lastNameU);
-    explicit accountHolder(const char* nameU, const char* lastNameU);  //define account holder only by name
+    explicit accountHolder(const char* nameU, const char* lastNameU);  //define account holder only by name and last name
     //Member functions
-    char* getFullName()          const;     //Returns the full name of the account holder
-    char* getAddress()           const;     //Returns the address of the account holder
-    char* getSSN()               const;     //Returns the SSN of the account holder
-    double      getCreditScore() const;     //Returns the credit score of the account holder
-    double*     getAccounts()    const;     //Returns the account numbers associated with this holder
-    void addAccount(double accountnumber);  //Adds an additional account associated with the account holder
+    const char* getFullName();                //Returns the full name of the account holder
+    const char* getAddress();                 //Returns the address of the account holder
+    const char* getSSN();                     //Returns the SSN of the account holder
+    double getCreditScore();                  //Returns the credit score of the account holder
+    double getAccount();                      //Returns the account number associated with this holder
+    void addAccount(double accountNumber);    //Adds an account associated with the account holder
     //Set Personal information functions
-    void setAddress(const char* address);
-    void setSSN(const char* SSN);
-    void setCreditScore(double Score);
-    void setAccounts(double* arrayAccounts);
+    void setAddress(const char* addressIn);
+    void setSSN(const char* SSNIn);
+    void setCreditScore(double ScoreIn);
 
 private:
-    const char* name;
-    const char* middleInit;
-    const char* lastName;
-    const char* address;
-    const char* SSN;
-    double Score;
-    double* arrayAccounts;
+    std::string name;
+    std::string middleInit;
+    std::string lastName;
+    std::string address;
+    std::string SSN;
+    double score;
+    double account;
 
 };
