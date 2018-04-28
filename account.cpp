c3aaf73b7d7565ddad71b6bd6e223c25a691f296
Dennis Soasti <soastid@gmail.com>
2018-04-27 21:29:24 -0400

    Finished main classes. Defined all member equations. Need to review with team for adaptation

--- account.cpp
+++ account.cpp
@@ -2,3 +2,42 @@
 // Created by Dennis on 4/24/2018.
 //
 
+#include <iostream>
+#include <string>
+
+#include "account.h"
+#include "accountHolder.h"
+#include <string>
+
+using namespace std;
+
+account::account()
+{
+    accountNumber = 0;
+    balance = 0;
+    userID = " ";
+    userPassword = " ";
+}
+
+account::account(double accountNumberIn, double balanceIn, const char *userIDIn, const char *userPasswordIn)
+{
+    accountNumber = accountNumberIn;
+    balance = balanceIn;
+    userID = userIDIn;
+    userPassword = userPasswordIn;
+}
+
+void account::deposit(double accountNumber, double depositAmount)
+{
+    balance += depositAmount;
+}
+
+void account::withdrawal(double accountNumber, double withdrawAmount)
+{
+    balance -= withdrawAmount;
+}
+
+void account::linkAccount(double accountNumber, accountHolder a)
+{
+    a.addAccount(accountNumber);
+}
