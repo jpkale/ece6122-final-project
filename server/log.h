2f45e6c1ec036c82046bce64220ed0401cb6f7e4
rafzal123 <38430153+rafzal123@users.noreply.github.com>
2018-04-29 15:42:47 -0500

    Add files via upload
    added log functions with encryption use

---
+++ server/log.h
@@ -0,0 +1,15 @@
+//
+// Created by Rimza Afzal
+//
+
+#include <string>
+
+using namespace std;
+
+class log{
+public:
+    static void createFile(string user, string password);
+    static void write(string user, string password, int balance, int amount, int flag);
+    static int getBalance(string user, string password);
+    static string getPassword(string user);
+};
