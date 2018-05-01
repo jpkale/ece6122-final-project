1dc62dbb089a6a533f0a566e965ea18d86fe226c
Nick Dargi <ndargi@bu.edu>
2018-04-30 21:32:19 -0400

    Added Log Functions to Server

--- server/server.cpp
+++ server/server.cpp
@@ -9,6 +9,7 @@
 #include "../Sockets/CreateServer/ServerFunc.h"
 #include "../Sockets/Serialization/Serialize.h"
 #include <sstream>
+#include "../server/log.h"
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         cout << "Usage: ./BankingAppClient [PORT]" << endl;
@@ -16,9 +17,10 @@ int main(int argc, char *argv[]) {
     }
 
     int socketID, n;
-    bool was_successful;
+    bool was_successful = false;
     double balance;
     double amount;
+    std::string passCheck;
     char buffer[256];
     int b = 1;
     socketID = servercreator(argv[1]);
@@ -38,6 +40,7 @@ int main(int argc, char *argv[]) {
                 //Attempt to create account and set the value of was_successful.
                 username = serverreturnusername(buffer);
                 password = serverreturnpassword(buffer);
+                log::createFile(username,password);
                 if (was_successful)
                 {
                     n = write(socketID,"1",1);//Will Send True if successful
@@ -50,9 +53,17 @@ int main(int argc, char *argv[]) {
                 //Attempt to Perform Deposit of amount here and set boolean was_successful, set balance to accounts balance
                 username = serverreturnusername(buffer);
                 password = serverreturnpassword(buffer);
+                passCheck = log::getPassword(username, password);
                 amount = serverreturnamount(buffer);
+                if(passCheck == password)
+                {
+                    was_successful = true;
+                }
                 if (was_successful)
                 {
+                    double newamount = log::getBalance(username, password) + amount;
+                    log::writeFile(username, password, newamount, amount, 1);
+                    balance = log::getBalance(username, password);
                     strs << balance;
                     std::string sendstring = "1," + strs.str() + ",";
                     strcpy(buffer,sendstring.c_str());
@@ -69,9 +80,19 @@ int main(int argc, char *argv[]) {
                 //Attempt to Perform Withdrawl here and set boolean was_successful, set balance to accounts balance
                 username = serverreturnusername(buffer);
                 password = serverreturnpassword(buffer);
+                passCheck = log::getPassword(username, password);
                 amount = serverreturnamount(buffer);
+                double oldbalance = log::getBalance(username, password);
+                if(passCheck == password && (oldbalance >= amount))
+                {
+                    was_successful = true;
+                }
+
                 if (was_successful)
                 {
+                    double newamount = log::getBalance(username, password) - amount;
+                    log::writeFile(username, password, newamount, amount, 1);
+                    balance = log::getBalance(username, password);
                     strs << balance;
                     std::string sendstring = "1," + strs.str() + ",";
                     strcpy(buffer,sendstring.c_str());
@@ -85,11 +106,19 @@ int main(int argc, char *argv[]) {
                     n = write(socketID,buffer,strlen(buffer));///Will Send False if unsuccessful followed by balance
                 }
             case 4: printf("Login to Account");
+
                 //Attempt to Perform Login here and set boolean was_successful, set balance to accounts balance
                 username = serverreturnusername(buffer);
                 password = serverreturnpassword(buffer);
+                passCheck = log::getPassword(username, password);
+
+                if(passCheck == password)
+                {
+                    was_successful = true;
+                }
                 if (was_successful)
                 {
+                    balance = log::getBalance(username, password);
                     strs << balance;
                     std::string sendstring = "1," + strs.str() + ",";
                     strcpy(buffer,sendstring.c_str());
